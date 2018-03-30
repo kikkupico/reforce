f4e8857e689385c20eb3c001ac429b1728fd97dd
Ram <ramaveer@cisco.com>
2018-03-30 18:37:17 +0530

    npm packaging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as d3 from 'd3-force'
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const DefaultLinkComponent = props => <line
+            x1={props.link.source.x}
+            y1={props.link.source.y}
+            x2={props.link.target.x}
+            y2={props.link.target.y}            
+            stroke="#333"            
+            strokeWidth="0.5"
+            />
+
+const DefaultNodeComponent = props => <div style={{ backgroundColor:props.node.color, borderRadius: '50%', width: `${props.node.size}px`, height: `${props.node.size}px`}} />
+
+export default class ReForce extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = state = {
+        nodes: [],
+        links: [],
+      }
+    }
+    
+    componentDidMount() {    	
+        this.setState({ nodes: this.props.nodes, links: this.props.links }, () => {        
+            this.simulation = d3.forceSimulation(this.state.nodes)
+                .force("charge",
+                    d3.forceManyBody()
+                    .strength(this.props.forceStrength)
+                )
+                .force("link",
+                    d3.forceLink().distance(this.props.linkDistance).links(this.state.links)
+                )
+                .force("x", d3.forceX(this.props.width / 2))
+                .force("y", d3.forceY(this.props.height / 2))
+                .force('collision', d3.forceCollide().radius(node => node.size/2)
+                  );
+
+            this.simulation.on('tick', () => this.setState({
+                links: this.state.links,
+                nodes: this.state.nodes
+            }))
+        })
+    }
+
+    render() {
+    	return (
+      <div style={{position:"relative", width:`${this.props.width}px`, height:`${this.props.height}px`, overflow:"hidden"}}>
+      <svg width={`${this.props.width}px`} height={`${this.props.height}px`} style={{position:"absolute"}}>
+      {this.state.links.map((link, index) =>
+        React.cloneElement(this.props.linkComponent ? this.props.linkComponent:<DefaultLinkComponent />, {link:link, key:`link-${index}`})        
+        )
+      }
+      </svg>
+      <div style={{position:"absolute"}}>      
+      {
+        this.state.nodes.map((node, index) => 
+          <div style={{ position:'absolute', top:node.y-node.size/2, left:node.x-node.size/2}}>
+          {            
+            React.cloneElement(this.props.nodeComponent?this.props.nodeComponent:<DefaultNodeComponent />, {node:node, key:index})
+          }
+          </div>
+        )
+      }      
+      </div>
+      </div>
+    )
+    }
+}
+
+ReForce.defaultProps = {
+  width: 1300,
+  height: 800,
+  linkDistance: 70,
+  forceStrength: -300
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'lib');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const WebpackConfig = {
+
+    entry: APP_DIR + '/index.js',
+
+    output: {
+        path: BUILD_DIR,
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        library: 'reforce'
+    },
+
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /.js$/,
+                exclude: /node_modules/,
+                include : APP_DIR,
+                options: {
+                    presets: [ 'env', 'react' ]
+                }
+            }
+        ],
+    },
+
+}
+
+
+// webpack production config.
+if ( process.env.NODE_ENV === 'production' ) {
+
+    WebpackConfig.externals = {
+        'react': 'react',
+        'react-dom': 'react-dom'        
+    };
+
+    WebpackConfig.plugins = [
+        new webpack.optimize.AggressiveMergingPlugin(),        
+    ];
+
+}
+
+
+module.exports = WebpackConfig;
